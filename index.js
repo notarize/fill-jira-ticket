75d27d5014e942062b62f9503dbca98027e48b5e
Trevor Morris <83480042+gotrevor-notarize@users.noreply.github.com>
2022-12-13 10:14:15 -0500

    [DEVOPS-2931] Upgrade to Node 16 (#3)

--- index.js
+++ index.js
@@ -24,8 +24,12 @@ async function run() {
     }
     const jiraTicketKey = matches[0];
     core.info(`Jira Ticket Key: ${jiraTicketKey}`);
-    const body = github.context.payload.pull_request.body;
-    core.info(body);
+    let body = github.context.payload.pull_request.body;
+    // core.info(body);
+    if (!body) {
+      core.warning('Body is blank.  Jira ticket will over-write any previous body.');
+      body = ""
+    }
     if (body.includes(jiraTicketKey)) {
       core.info('PR body is prefixed already - no updates made');
       return;
@@ -37,21 +41,21 @@ async function run() {
       pull_number: github.context.payload.pull_request.number,
     }
 
-    linkRegex = /^(?=.*?\bJIRA\b)(?=.*?\bticket\b).*$/m
-    lineToAdd = `:ticket: [JIRA ticket](https://notarize.atlassian.net/browse/${jiraTicketKey})`
-    lineExists = body.match(linkRegex)
+    const linkRegex = /^(?=.*?\bJIRA\b)(?=.*?\bticket\b).*$/m
+    let lineToAdd = `:ticket: [JIRA ticket: ${jiraTicketKey}](https://notarize.atlassian.net/browse/${jiraTicketKey}) :ticket:`
+    const lineExists = body.match(linkRegex)
     if (lineExists) {
-      core.info("Line exists in PR body without ticket");
+      core.info("'JIRA ticket' exists in PR body without ticket number; replacing that line.");
       request.body = body.replace(linkRegex, lineToAdd)
     } else {
-      core.info("Adding line to PR body");
+      core.info("'JIRA ticket' not found.  Adding line to PR body.");
       request.body = lineToAdd.concat('\n', body);
     }
 
     core.debug(`new body: ${request.body}`)
 
-    const client = new github.GitHub(inputs.token);
-    const response = await client.pulls.update(request);
+    const client = github.getOctokit(inputs.token);
+    const response = await client.rest.pulls.update(request);
 
     core.info(`response: ${response.status}`);
     if (response.status !== 200) {
