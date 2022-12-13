b4f46384fa650e5a9219184cc3c1cec756315d85
Trevor Morris <trevor.morris@notarize.com>
2022-12-12 23:53:57 -0500

    update dependencies

---
+++ bin/rebuild-for-deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -ex
+script_dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+cd $script_dir || exit 1
+cd .. || exit 1
+
+rm -rf node_modules
+rm -f package-lock.json
+npm install -omit=dev
