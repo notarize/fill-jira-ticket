3e4b7e3ea38ef6c17636a43f21cd10a2c630b56b
Trevor Morris <trevor.morris@notarize.com>
2024-05-03 10:52:40 -0400

    [DEVOPS-6304] upgrade fill jira ticket to node 20 (#6)

--- bin/rebuild-for-deploy.sh
+++ bin/rebuild-for-deploy.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 set -ex
 script_dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
-cd $script_dir || exit 1
+cd "$script_dir" || exit 1
 cd .. || exit 1
 
 rm -rf node_modules
 rm -f package-lock.json
 npm install -omit=dev
+npm dedup -omit=dev
